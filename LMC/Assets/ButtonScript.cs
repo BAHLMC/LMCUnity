1140e3ed68dc36785d22cda85f220c63887f8ba1
kendrickjames <kjames@trinity.edu>
2015-09-24 03:14:48 -0500

    LMC Basics
    Created a lot of simple things. The basic grid, input, output, and such
    are there. The scripts exist but don't have much. I created a button in
    one of the scripts in case we decide to go that way instead of just
    having sprites.

---
+++ LMC/Assets/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.Button(new Rect(200, 150, 100, 50), "Main Menu");
+    }
+}
