a718bb5b46f7103bf52bfd13b9cf4a1ed93763d3
cjpeake9 <cpeake@trinity.edu>
2015-10-01 08:47:26 -0500

    Options Screen
    Fixed Anchors
    Fixed Scaling - mostly

---
+++ LMC/Assets/Scripts/OptButtons.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void GoToGame () {
+		Application.LoadLevel(1);
+	}
+
+    public void GoToMainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void QualityLow()
+    {
+        
+    }
+
+    public void QualityMed()
+    {
+        
+    }
+
+    public void QualityHigh()
+    {
+        
+    }
+}
