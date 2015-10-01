ce92842e0dab51a57f3ce679ee4883be4b42e6b8
kendrickjames <kjames@trinity.edu>
2015-10-01 03:14:28 -0500

    LMC Screen Updates
    Added script transition functionality and set up a base shell for the
    script that includes some empty functions some simple linking between
    functions and a couple global variables that will likely prove necessary
    in the future.

--- LMC/Assets/Scripts/LMCScript.cs
+++ LMC/Assets/Scripts/LMCScript.cs
@@ -1,15 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LMCScript : MonoBehaviour {
 
+    // Private booleans for switching between modes
+    private bool isRunning;
+    private bool inEditMode;
+
+    //Interactive variables set in Unity
+    public Text parsedTextBox;
+    public InputField scriptInput;
+
 	// Use this for initialization
 	void Start () {
-	
+        parsedTextBox = GetComponent<Text>();
+        scriptInput = GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        scriptInput.enabled = inEditMode;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void fixedUpdate()
+    {
+        if (isRunning)
+            doNextStep();
+    }
+
+    void doNextStep()
+    {
+
+    }
+
+
+    public void onEditClicked()
+    {
+        inEditMode = true;
+    }
+
+    public void onSaveClicked()
+    {
+        inEditMode = false;
+        // updateParsedText;
+    }
+
+    public void onPlayPauseClicked()
+    {
+        isRunning = !isRunning;
+    }
+
+    public void onRunClicked()
+    {
+        isRunning = true;
+    }
+
+    public void onResetClicked()
+    {
+        isRunning = false;
+    }
+
+    public void onStepClicked()
+    {
+        isRunning = false;
+        doNextStep();
+    }
+
+
+
 }
