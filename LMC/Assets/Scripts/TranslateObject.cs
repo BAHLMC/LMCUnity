87f49afe82696cbf415e3c92a17bf51af50572bd
cjpeake9 <cpeake@trinity.edu>
2015-10-08 01:32:18 -0500

    Started playing with animation
    Teeny Tiny script but it took me a while to figure out. Also a great
    starting point.

---
+++ LMC/Assets/Scripts/TranslateObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TranslateObject : MonoBehaviour {
+
+    public GameObject target;
+    public Vector2 startLocation;
+    public Vector2 endLocation;
+    private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        target.transform.localPosition = Vector3.Lerp(new Vector3(startLocation.x, startLocation.y, target.transform.position.z), new Vector3(endLocation.x, endLocation.y, target.transform.position.z), Time.time);
+	}
+}
