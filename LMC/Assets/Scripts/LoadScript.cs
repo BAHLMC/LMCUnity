853472450162c39cc45de6001e9b4432d6380e92
Bryan Schrock <bryanrschrock@gmail.com>
2016-04-24 18:18:06 -0500

    Fixes anchors and some errors in compiling

--- LMC/Assets/Scripts/LoadScript.cs
+++ LMC/Assets/Scripts/LoadScript.cs
@@ -174,10 +174,15 @@ public class LoadScript : MonoBehaviour
 
 	static string convertRegToString (int reg)
 	{
-		if (reg < 10) {
-			return "0" + reg.ToString ();
-		} else {
-			return reg.ToString ();
+		try{
+			if (reg < 10) {
+				return "0" + reg.ToString ();
+			} else {
+				return reg.ToString ();
+			}
+		} catch (KeyNotFoundException e){
+			Debug.Log ("Key not found");
+			return "-1";
 		}
 	}
 
@@ -203,38 +208,49 @@ public class LoadScript : MonoBehaviour
 		for (int i = 0; i <al.Length; i++) {
 			if (al [i].Contains ("DAT")) {
 				string[] arr = al [i].Split (' ');
-				dats [arr [0]] = i;
+				if(arr[0].Contains("DAT")){
+					dats [arr [1]] = i;
+				}else if(arr[1].Contains("DAT")){
+					dats [arr [0]] = i;
+				}
 			}
 		}
 		string[] ret = new string[al.Length];
 		for (int i = 0; i < al.Length; i++) {
-			string s = al [i];
-			string[] arr = s.Split (' ');
-			if (arr [0] == "HLT")
-				ret [i] = ("000");
-			else if (arr [0] == "INP")
-				ret [i] = ("901");
-			else if (arr [0] == "OUT")
-				ret [i] = ("902");
-			else if (arr [0] == "ADD") {
-				ret [i] = "1" + convertRegToString (dats [arr [1]]);
-			} else if (arr [0] == "SUB") {
-				ret [i] = "2" + convertRegToString (dats [arr [1]]);
-			} else if (arr [0] == "STA") {
-				ret [i] = "3" + convertRegToString (dats [arr [1]]);
-			} else if (arr [0] == "LDA") {
-				ret [i] = "5" + convertRegToString (dats [arr [1]]);
-			} else if (arr [0] == "BRA") {
-				ret [i] = "6" + convertRegToString (dats [arr [1]]);
-			} else if (arr [0] == "BRZ") {
-				ret [i] = "7" + convertRegToString (dats [arr [1]]);
-			} else if (arr [0] == "BRP") {
-				ret [i] = "8" + convertRegToString (dats [arr [1]]);
-			} else if (arr.Length <= 1 || arr[1] != "DAT")
-            {
-                //If it is none of the above commands and not related to a DAT register, we will return this erro code
-                ret[i] = "-1";
-            }
+			try{
+				string s = al [i];
+				string[] arr = s.Split (' ');
+				if (arr [0] == "HLT")
+					ret [i] = ("000");
+				else if (arr [0] == "INP")
+					ret [i] = ("901");
+				else if (arr [0] == "OUT")
+					ret [i] = ("902");
+				else if (arr [0] == "ADD") {
+					ret [i] = "1" + convertRegToString (dats [arr [1]]);
+				} else if (arr [0] == "SUB") {
+					ret [i] = "2" + convertRegToString (dats [arr [1]]);
+				} else if (arr [0] == "STA") {
+					ret [i] = "3" + convertRegToString (dats [arr [1]]);
+				} else if (arr [0] == "LDA") {
+					ret [i] = "5" + convertRegToString (dats [arr [1]]);
+				} else if (arr [0] == "BRA") {
+					ret [i] = "6" + convertRegToString (dats [arr [1]]);
+				} else if (arr [0] == "BRZ") {
+					ret [i] = "7" + convertRegToString (dats [arr [1]]);
+				} else if (arr [0] == "BRP") {
+					ret [i] = "8" + convertRegToString (dats [arr [1]]);
+				}else if(arr[0]  == ""){
+
+				}
+				else if (arr.Length <= 1 || arr[1] != "DAT")
+            	{
+	                //If it is none of the above commands and not related to a DAT register, we will return this erro code
+                	ret[i] = "-1";
+            	}
+			}catch(KeyNotFoundException e){
+				ret[i] = "-1";
+			}
 
 		}
 		return ret;
