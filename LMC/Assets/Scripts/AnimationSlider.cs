81dcd9942d185a618e4b0231412e367a83c890b4
kendrickjames <kjames@trinity.edu>
2016-04-24 19:29:44 -0500

    Presentation Changes
    Changed a lot of text box sizes. Also ensured that the slider works.

--- LMC/Assets/Scripts/AnimationSlider.cs
+++ LMC/Assets/Scripts/AnimationSlider.cs
@@ -4,22 +4,18 @@ using UnityEngine.UI;
 
 public class AnimationSlider : MonoBehaviour {
 
-    public GameObject AnimSlider;
+	public GameObject AnimSlider;
+	public InputField AnimText;
 
 	// Use this for initialization
 	void Start () {
 		if (PlayerPrefs.HasKey ("Animation Speed"))
 			AnimSlider.GetComponent<Slider> ().value = PlayerPrefs.GetFloat ("Animation Speed");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        gameObject.transform.GetChild(1).GetComponent<Text>().text = AnimSlider.GetComponent<Slider>().value + "";
-       
-	}
 
     public void setPlayerPref()
     {
         PlayerPrefs.SetFloat("Animation Speed", AnimSlider.GetComponent<Slider>().value);
-    }
+		AnimText.text = AnimSlider.GetComponent<Slider>().value + "";
+	}
 }
